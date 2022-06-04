0ecdb18cff2f9e66f64acf32b897b8125a85f38b
thangpn <thangpn1412@its-global.vn>
2022-06-04 17:35:12 +0700

    init init

---
+++ src/main/java/com/example/chatchit/ChatchitApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatchit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatchitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatchitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatchit/ChatchitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatchit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatchitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
