d0cfd763a360b369e7f48ba309286927fb3defd9
thangpn <thangpn1412@its-global.vn>
2022-07-02 17:36:20 +0700

    init init

---
+++ src/main/java/com/example/chatchit/api/EndChat.java
@@ -0,0 +1,57 @@
+package com.example.chatchit.api;
+
+import com.example.chatchit.model.UserAtt;
+import com.example.chatchit.model.Users;
+import com.example.chatchit.repo.UserRepository;
+import com.example.chatchit.service.SendMessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class EndChat {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SendMessageService sendMessageService;
+
+    private static final Logger logger = LoggerFactory.getLogger(EndChat.class);
+
+    @PostMapping("/endChat")
+    public Boolean startChat(@RequestParam String id) {
+        logger.info("end chat with id = {}" + id);
+        Optional<Users> usedb = userRepository.findFirstByFbId(id);
+        if (usedb.isPresent()) {
+            if (usedb.get().getConnectId() != null) {
+                endChatForUser(usedb.get().getConnectId());
+                usedb.get().setConnectId(null);
+                userRepository.save(usedb.get());
+                return true;
+            } else {
+                sendMessageService.sendTexToMe("Bạn chưa kết được kết nối với người lạ");
+            }
+        }
+        return false;
+    }
+
+    private boolean endChatForUser(String id) {
+        Optional<Users> usedb = userRepository.findFirstByFbId(id);
+        if (usedb.isPresent()) {
+            if (usedb.get().getConnectId() != null) {
+                usedb.get().setConnectId(null);
+                userRepository.save(usedb.get());
+                return true;
+            }
+        }
+        return false;
+    }
+}
